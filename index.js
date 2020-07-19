7573c0de1ac523a03b909056b6f630747ed5f658
Huazhi-Fang <hzfang80@gmail.com>
2020-07-18 22:57:02 -0400

    third time

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore() {
+// Library Code
+function createStore(reducer) {
     // The store should have four parts
     // 1. The state
     // 2. Get the state
@@ -6,20 +7,42 @@ function createStore() {
     // 4. Update the state
 
     let state
-    let listeners =[]
+    let listeners = []
 
     const getState = () => state
 
-    const subscribe = (lister) => {
+    const subscribe = (listener) => {
         listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
     }
 
     return {
-        getState
+        getState,
+        subscribe,
+        dispatch
     }
 }
 
-const store = createStore()
-store.subscribe(() => {}
+// App Code
+function todos (state = [], action) {
+    if(action.type === 'ADD_TODO') {
+        return state.concat([action.todo])
+    }
+
+    return state
+}
+
+const store = createStore(todos)
+store.subscribe(() => {
     console.log('The new state is: ', store.getState())
+})
+const ussubscribe = store.subscribe(() => {
+    console.log('The store changed.')
 })
